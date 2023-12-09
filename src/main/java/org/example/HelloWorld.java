e5d833dd22bac445369cc2811d7aab86c0cd2d1e
ekambaram <eka290402@gmail.com>
2023-12-09 11:54:50 +0530

    09-11-23

--- src/main/java/org/example/HelloWorld.java
+++ src/main/java/org/example/HelloWorld.java
@@ -1,4 +1,11 @@
 package org.example;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.sql.SQLOutput;
+
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello Ekambaram");
+    }
 }
